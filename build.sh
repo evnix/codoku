9f74c28421c2ad60ba10b773a926919e637659fd
evnix <evnix.com@gmal.com>
2017-07-30 14:05:14 +0100

    new faster binaries

--- build.sh
+++
@@ -1,4 +0,0 @@
-env GOOS=darwin GOARCH=amd64 go build -o bin/codoku_darwin_amd64 codoku.go 
-env GOOS=linux GOARCH=amd64 go build -o bin/codoku_linux_amd64 codoku.go 
-env GOOS=windows GOARCH=amd64 go build -o bin/codoku_windows_amd64.exe codoku.go 
-
